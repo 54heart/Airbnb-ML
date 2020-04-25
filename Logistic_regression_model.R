1cea9c52a2261d6402ebe07d40c175cb97993197
Aishwarya Bhangale <aishwaryabhangale1997@gmail.com>
2020-04-25 17:37:14 -0400

    Added few lines

--- Logistic_regression_model.R
+++ Logistic_regression_model.R
@@ -6,25 +6,33 @@ df = read.csv("train_cleaned.csv")
 df_competition = read.csv("test_cleaned.csv")
 View(df_competition)
 ## Randomly partition the data into 30% testing data and the remaining 70% data.
-test_instn = sample(nrow(df), 0.3*nrow(df))
+test_instn = sample(nrow(df), 0.25*nrow(df))
 df_test <- df[test_instn,]
 ## Save the rest of the data as the data that isn't testing
 df_rest <- df[-test_instn,]
 ## e. Randomly partition the remaining data into 75% training data and 25% validation data.
-valid_instn = sample(nrow(df_rest), 0.25*nrow(df_rest))
+valid_instn = sample(nrow(df_rest), 0.3*nrow(df_rest))
 df_valid <- df_rest[valid_instn,]
 df_train <- df_rest[-valid_instn,]
+#View(df_competition)
 
-model_log <- glm(high_booking_rate~accommodates+availability_60+availability_90
-                 +bathrooms+Real_Bed+bedrooms+beds+
-                   cancellation_policy+cleaning_fee+extra_people+
-                   first_review+guests_included+host_about+host_has_profile_pic+
-                   host_identity_verified+host_is_superhost+host_listings_count+
-                   host_response_rate+host_response_time+instant_bookable+
-                   is_business_travel_ready+is_location_exact+maximum_nights+
-                   minimum_nights+price+require_guest_phone_verification+
-                   require_guest_profile_picture+requires_license+
-                   experience, data = df_train,family="binomial")
+model_log <- glm(high_booking_rate~accommodates+ amenities_count+
+                   availability_365+ availability_60+availability_90+
+                   bathrooms+ Real_Bed+ bedrooms+ beds+ 
+                   cancellation_policy+ cleaning_fee+ extra_people+
+                   first_review+ guests_included+ host_about+
+                   host_has_profile_pic+ host_identity_verified+
+                   host_is_superhost+ host_listings_count+
+                   host_response_rate+ host_response_time+ experience+
+                   instant_bookable+ is_business_travel_ready+ 
+                   is_location_exact+ long_stay+ minimum_nights+
+                   price+ propertyApartment+ propertyCommon_house+
+                   propertySide_house+ propertyHotel+ propertySpecial+
+                   require_guest_phone_verification+ 
+                   require_guest_profile_picture+ requires_license+
+                   roomEntire.home.apt+ roomPrivate.room+
+                   roomShared.room+ security_deposit+ flexible
+                 , data = df_train,family="binomial")
 summary(model_log)
 
 
@@ -58,21 +66,34 @@ confusion_matrix <- function(preds, actuals, cutoff){
 }
 
 #log_valid_preds = predict(model_log,newdata=df_valid,type="response")
-log_matrix <- confusion_matrix(log_valid_probs, df_valid$high_booking_rate,0.448829)
+log_matrix <- confusion_matrix(log_valid_probs, df_valid$high_booking_rate,0.4844703)
 
 acc_log = (log_matrix[1] + log_matrix[4])/sum(log_matrix)
 acc_log
-#0.7788588
-
+#0.7856666
 #Trying to improve the model
 log_matrix
 
-log_com_preds = predict(model_log,newdata=df_competition,type="response")
-#log_matrix <- confusion_matrix(log_test_preds, df_competition$high_booking_rate,.5)
-library(ROCR)
-pred <- prediction(log_com_preds,df_test$high_booking_rate)
+#Now, fitting that to our test data subsetted from the training data
+log_test_probs <- predict(model_log, newdata = df_test, type = "response")
+log_matrix <- confusion_matrix(log_test_probs, df_test$high_booking_rate,0.4353808)
+acc_log = (log_matrix[1] + log_matrix[4])/sum(log_matrix)
+acc_log
+#0.7848243
+
+#
+log_test_probs = predict(model_log,newdata=df_competition,type="response")
+log_test_probs
+length(log_test_probs)
+length(df_competition$high_booking_rate)
+#pred <- prediction(log_test_probs, df_competition$high_booking_rate)
+
+#log_matrix <- confusion_matrix(log_test_probs, df_competition$high_booking_rate,0.441866)
+#library(ROCR)
+#pred <- prediction(log_com_preds,df_test$high_booking_rate)
 #acc_log = (log_matrix[1] + log_matrix[4])/sum(log_matrix)
 #acc_log
+
 
 #test$probs <- predict(movies.pruned, newdata=movies_test)[,2]
 classpred <- ifelse(log_com_preds>.5,1,0)
